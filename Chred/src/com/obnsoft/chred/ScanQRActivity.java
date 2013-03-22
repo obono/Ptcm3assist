99b407c95633a8e2484afa089fdb375e9755cce3
obonokun <obonokun@53174389-d597-a8c4-652e-3103845fcc6d>
2013-03-22 05:45:13 +0000

    Extend 'MagnifyView' for importing QR-code.

--- Chred/src/com/obnsoft/chred/ScanQRActivity.java
+++ Chred/src/com/obnsoft/chred/ScanQRActivity.java
@@ -21,26 +21,26 @@ import java.io.InputStream;
 import java.util.zip.DataFormatException;
 import java.util.zip.Inflater;
 
+import com.obnsoft.view.MagnifyView;
+
 import jp.sourceforge.qrcode.QRCodeDecoder;
 import jp.sourceforge.qrcode.data.QRCodeImage;
 import jp.sourceforge.qrcode.exception.DecodingFailedException;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.RectF;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
 import android.view.View;
-import android.view.ScaleGestureDetector.OnScaleGestureListener;
+import android.view.animation.AlphaAnimation;
+import android.widget.TextView;
 
 public class ScanQRActivity extends Activity {
 
@@ -56,121 +56,30 @@ public class ScanQRActivity extends Activity {
     private int mTotalCnt;
     private int mTotalLen;
     private int mFinalLen;
+    private String mEname;
     private byte[] mMd5All = new byte[16];
     private byte[] mCmprsData;
 
     private boolean mIsOnResult;
     private Uri mUri;
     private Bitmap mBitmap;
+    private RectF mWorkRect = new RectF();
     private QRImageMediator mQrImage;
+    private MagnifyView mQrView;
+    private View mQrFrame;
 
     private Handler mTimeoutHandler = new Handler() {
-        @Override  
+        @Override
         public void dispatchMessage(Message msg) {
             if (msg.what == MSG_EXECSCAN) {
                 executeScan();
+                AlphaAnimation anim = new AlphaAnimation(0f, 1f);
+                anim.setDuration(1000);
+                anim.setFillAfter(true);
+                mQrFrame.startAnimation(anim);
             }
         }
     };
-
-    /*-----------------------------------------------------------------------*/
-
-    class MyView extends View implements OnScaleGestureListener {
-
-        private boolean mIsMoving;
-        private float mDrawX;
-        private float mDrawY;
-        private float mDrawScale = 1f;
-        private float mFocusX;
-        private float mFocusY;
-
-        private Paint mPaint = new Paint();
-        private ScaleGestureDetector mGestureDetector;
-
-        public MyView(Context context) {
-            super(context);
-            mPaint.setColor(Color.RED);
-            mPaint.setStyle(Paint.Style.STROKE);
-            mGestureDetector = new ScaleGestureDetector(context, this);
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-            if (mBitmap != null) {
-                canvas.save();
-                canvas.scale(mDrawScale, mDrawScale);
-                canvas.drawBitmap(mBitmap, mDrawX, mDrawY, null);
-                canvas.restore();
-                canvas.drawRect(0, 0, 200, 200, mPaint);
-                mQrImage.setTargetArea((int) -mDrawX, (int) -mDrawY,
-                        (int) (200 / mDrawScale), (int) (200 / mDrawScale));
-            }
-        }
-
-        @Override
-        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-            super.onSizeChanged(w, h, oldw, oldh);
-            mDrawX = 0f;
-            mDrawY = 0f;
-            mDrawScale = 1f;
-        }
-
-        @Override
-        public boolean onTouchEvent(MotionEvent event) {
-            boolean ret = false;
-            float x = event.getX();
-            float y = event.getY();
-            mGestureDetector.onTouchEvent(event);
-            switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                mIsMoving = true;
-                mFocusX = x;
-                mFocusY = y;
-                ret = true;
-                break;
-            case MotionEvent.ACTION_MOVE:
-                if (mIsMoving) {
-                    mDrawX += (x - mFocusX) / mDrawScale;
-                    mDrawY += (y - mFocusY) / mDrawScale;
-                    mFocusX = x;
-                    mFocusY = y;
-                    invalidate();
-                    ret = true;
-                }
-                break;
-            case MotionEvent.ACTION_CANCEL:
-            case MotionEvent.ACTION_UP:
-                if (mIsMoving) {
-                    mIsMoving = false;
-                    resetTimer();
-                    ret = true;
-                }
-                break;
-            }
-            return ret;
-        }
-
-        @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            mDrawScale *= detector.getScaleFactor();
-            invalidate();
-            return true;
-        }
-
-        @Override
-        public boolean onScaleBegin(ScaleGestureDetector detector) {
-            mIsMoving = false;
-            mFocusX = detector.getFocusX();
-            mFocusY = detector.getFocusY();
-            return true;
-        }
-
-        @Override
-        public void onScaleEnd(ScaleGestureDetector detector) {
-            resetTimer();
-        }
-    }
 
     /*-----------------------------------------------------------------------*/
 
@@ -208,8 +117,13 @@ public class ScanQRActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyView(this));
+        setContentView(R.layout.scanqr);
         mQrImage = new QRImageMediator();
+        mQrView = (MagnifyView) findViewById(R.id.view_qrimage);
+        mQrView.setScrollable(true);
+        mQrView.setScaleRange(.125f, 4f);
+        mQrView.setFrameColor(Color.TRANSPARENT);
+        mQrFrame = (View) findViewById(R.id.view_qrframe);
         mTotalCnt = 0;
     }
 
@@ -220,6 +134,7 @@ public class ScanQRActivity extends Activity {
                 InputStream in = getContentResolver().openInputStream(mUri);
                 mBitmap = BitmapFactory.decodeStream(in);
                 in.close();
+                mQrView.setBitmap(mBitmap, true);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -227,9 +142,7 @@ public class ScanQRActivity extends Activity {
             if (mIsOnResult) {
                 setCanceledResult();
             } else {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT/*ACTION_PICK*/);
-                intent.setType("image/*");
-                startActivityForResult(intent, REQUEST_ID_CHOOSE_FILE);
+                requsetFileFromGallery();
             }
         }
         super.onResume();
@@ -239,8 +152,9 @@ public class ScanQRActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
-        mTimeoutHandler.removeMessages(MSG_EXECSCAN);
+        stopTimer();
         if (mBitmap != null) {
+            mQrView.setBitmap(null, true);
             mBitmap.recycle();
             mBitmap = null;
         }
@@ -258,12 +172,46 @@ public class ScanQRActivity extends Activity {
         mIsOnResult = true;  // This function is called before onResume()
     }
 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        boolean ret = super.dispatchTouchEvent(ev);
+        if (mBitmap != null) {
+            switch (ev.getActionMasked()) {
+            case MotionEvent.ACTION_UP:
+                mQrView.getBitmapDrawRect(mWorkRect);
+                float scale = mBitmap.getWidth() / mWorkRect.width();
+                int size = Math.round(mQrFrame.getWidth() * scale);
+                mQrImage.setTargetArea((int) ((mQrFrame.getLeft() - mWorkRect.left) / scale),
+                        (int) ((mQrFrame.getTop() - mWorkRect.top) / scale), size, size);
+                startTimer();
+                break;
+            case MotionEvent.ACTION_DOWN:
+                stopTimer();
+                break;
+            }
+        }
+        return ret;
+    }
+
+    public void onClickOtherImage(View v) {
+        requsetFileFromGallery();
+    }
+
     /*-----------------------------------------------------------------------*/
 
-    private void resetTimer() {
-        myLog("resetTimer()");
-        mTimeoutHandler.removeMessages(MSG_EXECSCAN);
+    private void requsetFileFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT/*ACTION_PICK*/);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_ID_CHOOSE_FILE);
+    }
+
+    private void startTimer() {
+        stopTimer();
         mTimeoutHandler.sendEmptyMessageDelayed(MSG_EXECSCAN, MSEC_TIMEOUT_EXECSCAN);
+    }
+
+    private void stopTimer() {
+        mTimeoutHandler.removeMessages(MSG_EXECSCAN);
     }
 
     private void executeScan() {
@@ -296,8 +244,8 @@ public class ScanQRActivity extends Activity {
             mCurCnt = 0;
             mTotalCnt = qrData[3];
             System.arraycopy(qrData, 20, mMd5All, 0, 16);
-            // get name
-            // get type
+            mEname = Utils.extractString(partData, 9, 3).concat(":")
+                    .concat(Utils.extractString(partData, 0, 8));
             mTotalLen = Utils.extractValue(partData, 12, 4);
             mFinalLen = Utils.extractValue(partData, 16, 4);
             mCmprsData = new byte[mTotalLen];
@@ -318,7 +266,10 @@ public class ScanQRActivity extends Activity {
             return;
         }
         System.arraycopy(partData, 0, mCmprsData, mCurLen, partData.length);
+        mCurCnt++;
         mCurLen += partData.length;
+        TextView tv = (TextView) findViewById(R.id.text_qrinfo);
+        tv.setText(String.format("%s (%d/%d)", mEname, mCurCnt, mTotalCnt));
 
         /*  Last QR-code  */
         if (++mCurCnt == mTotalCnt) {
@@ -388,5 +339,7 @@ public class ScanQRActivity extends Activity {
 
     private void myLog(String str) {
         Log.d("CHRED", str);
+        TextView tv = (TextView) findViewById(R.id.text_qrmsg);
+        tv.setText(str);
     }
 }
