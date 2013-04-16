f22ca01ca1ab9e89d6a591bbc56f1d10fce72fc8
obonokun@gmail.com <obonokun@gmail.com@53174389-d597-a8c4-652e-3103845fcc6d>
2013-04-16 14:32:18 +0000

    Apply some drawable resources.

--- DiceCalendar/src/com/obnsoft/dicecalendar/MyWidgetProvider.java
+++ DiceCalendar/src/com/obnsoft/dicecalendar/MyWidgetProvider.java
@@ -22,13 +22,21 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.widget.RemoteViews;
 
 public class MyWidgetProvider extends AppWidgetProvider {
 
+    private static Bitmap sBitmap;
+
     @Override
     public void onEnabled(Context context) {
         super.onEnabled(context);
+        initalizeBitmap();
     }
 
     @Override
@@ -37,6 +45,8 @@ public class MyWidgetProvider extends AppWidgetProvider {
 
         RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget);
         ComponentName cn = new ComponentName(context, MyWidgetProvider.class);
+        drawBitmap();
+        rv.setImageViewBitmap(R.id.widget_image, sBitmap);
         Intent intent = new Intent(context, MainActivity.class);
         rv.setOnClickPendingIntent(R.id.widget_image,
                 PendingIntent.getActivity(context, 0, intent, 0));
@@ -46,6 +56,34 @@ public class MyWidgetProvider extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         super.onDisabled(context);
+        finishBitmap();
+    }
+
+    /*-----------------------------------------------------------------------*/
+
+    private void initalizeBitmap() {
+        if (sBitmap == null) {
+            sBitmap = Bitmap.createBitmap(512, 256, Bitmap.Config.ARGB_8888);
+        }
+    }
+
+    private void drawBitmap() {
+        if (sBitmap == null) {
+            initalizeBitmap();
+        }
+        Canvas canvas = new Canvas(sBitmap);
+        Paint paint = new Paint();
+        paint.setColor(Color.GRAY);
+        paint.setTextSize(64);
+        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+        canvas.drawText(String.valueOf(System.currentTimeMillis()), 0, 128, paint);
+    }
+
+    private void finishBitmap() {
+        if (sBitmap != null) {
+            sBitmap.recycle();
+            sBitmap = null;
+        }
     }
 
 }
